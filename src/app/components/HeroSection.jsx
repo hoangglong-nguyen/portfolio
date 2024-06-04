1ad94dab978d68d42800968efb18cfec6602f3fb
hoangglong-nguyen <148770033+hoangglong-nguyen@users.noreply.github.com>
2024-06-04 10:10:21 +0700

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,22 +35,7 @@ const HeroSection = () => {
             />
           </h1>
 
-          <div>
-            <Link
-              href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
-            <Link
-              href="NGUYEN-HOANG-LONG_Resume.pdf"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </Link>
-          </div>
+          
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
